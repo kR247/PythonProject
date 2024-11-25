63fdf9ca591e18591d749d08c69b479c14d61ffd
kR247 <131989025+kR247@users.noreply.github.com>
2024-11-25 19:32:02 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+number_of_complited_home_tasks = 12
+number_of_used_hours = 1.5
+name_of_course = 'Python'
+time_per_task = number_of_used_hours / number_of_complited_home_tasks
+print('Курс:',name_of_course,
+      ', всего задач:', number_of_complited_home_tasks,
+      ', затрачено часов:', number_of_used_hours,
+      ', среднее время выполнения:', time_per_task
+      )
